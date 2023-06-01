f869c84f01ee56cb9e3ee9dc5ed3e825960a0889
Laurie Roy <laurieroy.dev@gmail.com>
2023-06-01 17:07:01 -0500

    Add customTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React, { FC, ReactElement} from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import React, { FC, ReactElement } from 'react';
+import { customTheme } from './theme/customTheme';
 
-const App:FC = ():ReactElement => {
+const App: FC = (): ReactElement => {
   return (
-    <h1>
-      Hello world!
-    </h1>
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <h1>Hello world!</h1>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
