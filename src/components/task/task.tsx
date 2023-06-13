f17d30f90445d296e05c94b8cb8f4858167db235
Laurie Roy <laurieroy.dev@gmail.com>
2023-06-13 09:27:08 -0500

    Add task component, interface

---
+++ src/components/task/task.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@mui/material';
+import React, { FC, ReactElement } from 'react';
+
+export const Task: FC = (): ReactElement => {
+  return (
+    <Box
+      display="flex"
+      width="100%"
+      justify-content="flex-start"
+      flex-direction="column"
+      mb={2}
+      p={4}
+      sx={{
+        width: '100%',
+        backgroundColor: 'background.paper',
+        borderRadius: '8px',
+        border: '1px solid',
+        borderColor: 'error.light',
+      }}
+    >sdf</Box>
+  );
+};
--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -2,6 +2,7 @@ import React, { FC, ReactElement } from 'react';
 import { Box, Grid } from '@mui/material';
 import { format } from 'date-fns';
 import { TaskCounter } from '../taskCounter/taskCounter';
+import { Task } from '../task/task';
 
 export const TaskArea: FC = (): ReactElement => {
   return (
@@ -38,9 +39,9 @@ export const TaskArea: FC = (): ReactElement => {
           md={8}
           xs={10}
         >
-          <Box>Tasks go here</Box>
-          <Box>Tasks go here</Box>
-          <Box>Tasks go here</Box>
+          <Task />
+          <Task />
+          <Task />
         </Grid>
       </Grid>
     </Grid>
